f52155a776557c45bb0ab83e52f494e09700d574
Hannah Hall <hannahkhall13@gmail.com>
2018-01-22 09:17:50 -0600

    updates

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,33 @@
 angular.module('app', [])
-  .controller("ProjectsCtrl", function ($http) {
+    .controller("ProjectsCtrl", function ($http, $sce) {
     var vm = this;
     $http.get('/javascript/projects.json').then(
       function success(res) {
         vm.projects = res.data.projects;
         vm.techs = res.data.tech;
       });
+
+    vm.renderHtml = function (htmlCode) {
+        return $sce.trustAsHtml(htmlCode);
+    };
+    
+    vm.year = new Date().getFullYear();
+
+    function is_touch_device() {
+        return 'ontouchstart' in window;
+    }
+    let scroll = is_touch_device() ? 255 : 674
+
+    $(window).scroll(function () {
+
+        console.log($(window).scrollTop());
+        
+        if ($(window).scrollTop() > scroll) {
+            $('.navbar').addClass('fixed is-primary is-bold');
+        }
+
+        if ($(window).scrollTop() < scroll) {
+            $('.navbar').removeClass('fixed is-primary is-bold');
+        }
+    });
   });
